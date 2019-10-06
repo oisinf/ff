3fd1076c5804952506a16612a761358b53899c02
Oisin Feely <oisin.feely@viasat.com>
2019-10-06 20:01:18 +0100

    Proxy for axios

--- ff/src/Components/FFList/actions.ts
+++ ff/src/Components/FFList/actions.ts
@@ -1,1 +1,16 @@
-export const getFantasyFootballData = () => {};
+import axios from "axios";
+export const getFantasyFootballData = async () => {
+  const response = await axios({
+    method: "GET",
+    url: "api/bootstrap-static/"
+  })
+    .then(res => {
+      console.log("response", res);
+      return res;
+    })
+    .catch(err => {
+      console.log("error");
+      return null;
+    });
+  return response;
+};
